876aed7c31ccdaf6d73bfada996c6691380298e3
Margaret Morris <margaretblue@gmail.com>
2013-11-25 14:25:33 -0800

    init with pg, rspec database.yml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kikisdelivery::Application.config.secret_token = '6af2470927f7ba0923aa0a61ac2852cbae13e8c3b83ef742b866f5d3ec4ca63b038129e662c54d86c176c12abca15b7929aea0754da992c919ce87ce828b4269'
